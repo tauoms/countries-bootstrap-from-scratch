2f33d60629ff9b17ced0299c2fa9e41ff1f016b0
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-09-06 15:04:02 +0300

    Render country data

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,5 +1,46 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { initializeCountries } from "../services/countriesServices";
+import { Col, Container, Spinner } from "react-bootstrap";
+
 const Countries = () => {
-  return <div>Countries will be here</div>;
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => state.countries.countries);
+  const isLoading = useSelector((state) => state.countries.isLoading);
+
+  console.log("Countries: ", countries);
+  console.log("isLoading: ", isLoading);
+
+  useEffect(() => {
+    dispatch(initializeCountries());
+  }, [dispatch]);
+
+  // Handle the loading case here first (use Col, and Spinner)
+  if (isLoading) {
+    return <Spinner animation="border" />;
+  }
+
+  return (
+    <>
+      <div>Countries will be here</div>
+      {countries.map((country) => (
+        <Col className="text-center m-5" key={country.ccn3}>
+          <Container>
+            <img src={countries.flags.svg} alt="" />
+            <strong>{country.name.common}</strong>
+            <p>Capital: {country.capital}</p>
+          </Container>
+        </Col>
+      ))}
+      <Col className="text-center m-5">
+        <Container>
+          <strong>{}</strong>
+        </Container>
+      </Col>
+    </>
+  );
+
+  // Handle the received data case
 };
 
 export default Countries;
--- src/services/countriesServices.js
+++ src/services/countriesServices.js
@@ -1,10 +1,19 @@
 import axios from "axios";
+import { getCountries, isLoading } from "../store/countriesSlice";
 
 const baseUrl = 'https://restcountries.com/v3.1/all';
 
 const getAllCountries = async () => {
     const response = await axios.get(baseUrl);
     return response.data;
-}
+};
 
-export default { getAllCountries };
+const initializeCountries = () => {
+    return async (dispatch) => {
+        const countries = await getAllCountries();
+        dispatch(getCountries(countries));
+        dispatch(isLoading(false));
+    };
+};
+
+export { getAllCountries, initializeCountries };
