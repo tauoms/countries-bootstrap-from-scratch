806284a0f66b83e8cd51eefb5d408de48a4b0ff4
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-09-13 13:16:12 +0300

    Style displayed weather, fix temp to one decimal

--- src/components/CountrySingle.jsx
+++ src/components/CountrySingle.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Image, Spinner } from "react-bootstrap";
+import { Container, Image, Spinner } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
@@ -47,15 +47,15 @@ const CountrySingle = () => {
   // Show weather data here (min req: Temp, weather desciption, icon)
 
   return (
-    <>
-      <div>{weather.main.temp} °C</div>
-      <div>{weather.weather[0].description}</div>
+    <Container fluid className="mt-5 d-flex flex-column align-items-center">
+      <div>{weather.main.temp.toFixed(1)} °C</div>
       <div>
         <Image
           src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
         />
       </div>
-    </>
+      <div>{weather.weather[0].description}</div>
+    </Container>
   );
 };
 
