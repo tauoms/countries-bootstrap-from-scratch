2a943ab3da6ea83e2411861571c52242db12a8ef
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-09-20 10:53:07 +0300

    Install react-firebase-hooks to get useAuthState

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  // Below handles the auth state from Firebase
+  const [user, loading, error] = useAuthState(auth);
+
   return <div>Register will be here</div>;
 };
 
