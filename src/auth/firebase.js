c4247b0f1573318a83175d7c3bb1af93a2ec137d
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-09-20 10:29:43 +0300

    Create function loginWithEmailAndPassword

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,11 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // Remember to move apiKey to env file
+// Remember to move apiKey to env file
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "countries-react-58bde.firebaseapp.com",
   projectId: "countries-react-58bde",
@@ -16,3 +18,17 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const loginWithEmailAndPassword = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        alert(error.message);
+    }
+}
+
+export { auth, db, loginWithEmailAndPassword };
