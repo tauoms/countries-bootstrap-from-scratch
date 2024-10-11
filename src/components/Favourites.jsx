8966f6bd9987f253ff59cc3708c4123dbb03b0f9
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-10-11 10:15:10 +0300

    Create CountryCard component, use it on Favourites page

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -19,6 +19,7 @@ import {
   removeFavourite,
 } from "../store/favouritesSlice";
 import { Link } from "react-router-dom";
+import CountryCard from "./CountryCard";
 
 const Favourites = () => {
   const dispatch = useDispatch();
@@ -87,65 +88,7 @@ const Favourites = () => {
             country.name.common.toLowerCase().includes(search.toLowerCase())
           )
           .map((country) => (
-            <Col className="mt-5" key={country.name.official}>
-              <Card className="h-100 d-flex flex-column">
-                <Link
-                  to={`/countries/${country.name.common}`}
-                  state={{ country: country }}
-                >
-                  <Card.Img
-                    variant="top"
-                    src={country.flags.svg}
-                    alt={country.name.common}
-                    className="rounded h-50"
-                    style={{
-                      objectFit: "cover",
-                      minHeight: "200px",
-                      maxHeight: "200px",
-                    }}
-                  />
-                </Link>
-                <Card.Body className="d-flex flex-column justify-content-between">
-                  <div>
-                    <Card.Title>{country.name.common}</Card.Title>
-                    <Card.Subtitle className="mb-5 text-muted">
-                      {country.name.official}
-                    </Card.Subtitle>
-                  </div>
-                  <ListGroup variant="flush" className="flex-grow-1">
-                    <ListGroup.Item>
-                      <i className="h5 bi bi-people me-2" />
-                      {country.population.toLocaleString()}
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      <i className="h5 bi bi-cash-coin me-2" />
-                      {Object.values(country.currencies || {})
-                        .map(
-                          (currency) => `${currency.name} (${currency.symbol})`
-                        )
-                        .join(", ") || "No currency"}
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                      <i className="h5 bi bi-chat-dots me-2" />
-                      {Object.values(country.languages || {})
-                        .map((language) => language)
-                        .join(", ") || "No official language"}
-                    </ListGroup.Item>
-                  </ListGroup>
-                  <div className="mt-3">
-                    <Button
-                      variant="warning"
-                      className="w-100"
-                      onClick={() =>
-                        dispatch(removeFavourite(country.name.common))
-                      }
-                    >
-                      Remove Favourite
-                    </Button>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
+            <CountryCard key={country.name.common} country={country} />
           ))}
       </Row>
     </Container>
