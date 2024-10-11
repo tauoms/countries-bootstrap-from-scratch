e108d6cd06c4da7c6af6ad874a031af1b4ff19e8
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-10-11 12:36:21 +0300

    Change class to className

--- src/components/CountrySingle.jsx
+++ src/components/CountrySingle.jsx
@@ -60,7 +60,7 @@ const CountrySingle = (props) => {
           </div>
           <div>{weather.weather[0].description}</div>
           <div>
-            <i class="h5 bi bi-wind" />
+            <i className="h5 bi bi-wind" />
             {weather.wind.speed.toFixed(1)} m/s
           </div>
           <Button variant="light" onClick={() => navigate("/countries")}>
