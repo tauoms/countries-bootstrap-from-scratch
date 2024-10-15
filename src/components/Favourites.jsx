83348bdcffae4aff647917d458185996483cb707
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-10-15 19:24:19 +0300

    Add sorting to Favourites page

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -26,6 +26,9 @@ const Favourites = () => {
   let countriesList = useSelector((state) => state.countries.countries);
   let filteredCountries = [];
   const [search, setSearch] = useState("");
+  const [sortField, setSortField] = useState("population");
+  const [sortOrder, setSortOrder] = useState("desc");
+  const [activeSortButton, setActiveSortButton] = useState("population");
   const favouritesList = useSelector((state) => state.favourites.favourites);
   const favouritesLoading = useSelector(
     (state) => state.favourites.favouritesIsLoading
@@ -47,6 +50,17 @@ const Favourites = () => {
     dispatch(getFavouritesFromSource());
   }, [dispatch]);
 
+  // Toggle sorting field and order
+  const toggleSortOrder = (field) => {
+    if (sortField === field) {
+      setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+    } else {
+      setSortField(field);
+      setSortOrder("desc");
+    }
+    setActiveSortButton(field);
+  };
+
   if (countriesLoading || favouritesLoading) {
     return (
       <>
@@ -62,9 +76,66 @@ const Favourites = () => {
     );
   }
 
+  // Sorting logic
+  const sortedCountries = [...filteredCountries]
+    .filter((country) =>
+      country.name.common.toLowerCase().includes(search.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (sortField === "population") {
+        return sortOrder === "asc"
+          ? a.population - b.population
+          : b.population - a.population;
+      } else if (sortField === "area") {
+        return sortOrder === "asc" ? a.area - b.area : b.area - a.area;
+      } else if (sortField === "name") {
+        return sortOrder === "asc"
+          ? a.name.common.localeCompare(b.name.common, "fi")
+          : b.name.common.localeCompare(a.name.common, "fi");
+      }
+      return 0;
+    });
+
   return (
-    <Container className="container-lg" style={{ maxWidth: "1300px" }}>
-      <Row className="mt-5">
+    <Container className="container-lg mt-5" style={{ maxWidth: "1300px" }}>
+      <Row>
+        <Col className="mt-5 d-flex justify-content-center align-items-center">
+          {/* Sort Buttons */}
+          <div className="sort-by me-2">Sort by: </div>
+          <Button
+            onClick={() => toggleSortOrder("name")}
+            className={
+              activeSortButton === "name"
+                ? "button-bright me-3"
+                : "button-dark me-3"
+            }
+          >
+            Name {sortField === "name" && (sortOrder === "asc" ? "↑" : "↓")}
+          </Button>
+          <Button
+            onClick={() => toggleSortOrder("population")}
+            className={
+              activeSortButton === "population"
+                ? "button-bright me-3"
+                : "button-dark me-3"
+            }
+          >
+            Population{" "}
+            {sortField === "population" && (sortOrder === "asc" ? "↑" : "↓")}
+          </Button>
+          <Button
+            onClick={() => toggleSortOrder("area")}
+            className={
+              activeSortButton === "area"
+                ? "button-bright me-3"
+                : "button-dark me-3"
+            }
+          >
+            Area {sortField === "area" && (sortOrder === "asc" ? "↑" : "↓")}
+          </Button>
+        </Col>
+      </Row>
+      <Row>
         <Col className="mt-5 d-flex justify-content-center">
           <Form>
             <Form.Control
@@ -91,13 +162,9 @@ const Favourites = () => {
         </Col>
       </Row>
       <Row xs={1} md={3} lg={4} xl={5} xxl={5} className="g-3 mt-5">
-        {filteredCountries
-          .filter((country) =>
-            country.name.common.toLowerCase().includes(search.toLowerCase())
-          )
-          .map((country) => (
-            <CountryCard key={country.name.common} country={country} />
-          ))}
+        {sortedCountries.map((country) => (
+          <CountryCard key={country.name.common} country={country} />
+        ))}
       </Row>
 
       {!filteredCountries.length && (
